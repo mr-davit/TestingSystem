02cb7012c89bf8d5001a9f8f9dd6dc52a5ff3e45
ditokapanadze <ditokapanadze111@gmail.com>
2021-05-07 00:16:11 -0700

    scss fixed

--- js/profile.js
+++ js/profile.js
@@ -11,9 +11,9 @@ for (var i = 0; i < subjectLi.length; i++) {
 const students = document.querySelector(".students");
 const tests = document.querySelector(".tests");
 const teachers = document.querySelector(".teachers");
-const testsContent = document.querySelector(".tests_content");
-const studentsContent = document.querySelector(".students_content");
-const teachersContent = document.querySelector(".teachers_content");
+const testsSection = document.querySelector(".tests_section");
+const studentsSection = document.querySelector(".students_section");
+const teachersSection = document.querySelector(".teachers_section");
 
 const remove = (element) => {
   if (element.classList.contains("active")) {
@@ -24,17 +24,17 @@ const remove = (element) => {
 students.addEventListener("click", () => {
   remove(tests);
   remove(teachers);
-  testsContent.classList.add("hidden");
-  studentsContent.classList.remove("hidden");
+  testsSection.classList.add("hidden");
+  studentsSection.classList.remove("hidden");
   students.classList.add("active");
-  teachersContent.classList.add("hidden");
+  teachersSection.classList.add("hidden");
 });
 
 tests.addEventListener("click", () => {
-  studentsContent.classList.add("hidden");
-  testsContent.classList.remove("hidden");
+  studentsSection.classList.add("hidden");
+  testsSection.classList.remove("hidden");
   tests.classList.add("active");
-  teachersContent.classList.add("hidden");
+  teachersSection.classList.add("hidden");
   remove(students);
   remove(teachers);
 });
@@ -42,8 +42,8 @@ tests.addEventListener("click", () => {
 teachers.addEventListener("click", () => {
   remove(tests);
   remove(students);
-  studentsContent.classList.add("hidden");
-  testsContent.classList.add("hidden");
-  teachersContent.classList.remove("hidden");
+  studentsSection.classList.add("hidden");
+  testsSection.classList.add("hidden");
+  teachersSection.classList.remove("hidden");
   teachers.classList.add("active");
 });
